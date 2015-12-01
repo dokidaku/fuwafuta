eee47b786964f8b1dd4bfee73218944b19365ee8
Pisces000221 <1786762946@qq.com>
2015-12-01 23:57:11 +0800

    Sliding

--- DanmakuPool.cs
+++ DanmakuPool.cs
@@ -13,6 +13,8 @@ namespace TwoDoubleThree {
         public BulletType type;
         public int id;
         public long startTime, finishTime;
+        public double xStartPos;
+        public double xSpeed;
     }
 
     public class DanmakuPool : Form {
@@ -27,7 +29,7 @@ namespace TwoDoubleThree {
 
         private void InitializeComponent() {
             this.timer = new Timer();
-            timer.Interval = 10;
+            timer.Interval = 20;
             timer.Enabled = true;
             timer.Tick += Timer_Tick;
             timer.Start();
@@ -36,6 +38,7 @@ namespace TwoDoubleThree {
             this.TransparencyKey = this.BackColor;
         }
 
+        //private long lastTick;
         private void Timer_Tick(object sender, EventArgs e) {
             long now = DateTime.Now.Ticks;
             BulletInfo bif;
@@ -47,16 +50,27 @@ namespace TwoDoubleThree {
                     return;
                 }
             }
+            //int delta = (new TimeSpan(now - lastTick)).Seconds;
+            foreach (DictionaryEntry o in this.bullets) {
+                bif = (BulletInfo)(o.Value);
+                bif.bullet.Left =
+                    (int)(bif.xStartPos + bif.xSpeed * (now - bif.startTime) / TimeSpan.TicksPerSecond);
+            }
+            //lastTick = now;
         }
 
         public void Fire(BulletType type, String text, Color color) {
             BulletInfo bif = new BulletInfo();
-            int x = 20, y = 50;
+            int x = 20, y = 50 + lastID * 70;
+            double xStartPos = SystemInformation.WorkingArea.Size.Width;
+            double xSpeed = -120.0;
             bif.bullet = BulletDisp.Fire(text, color, x, y);
             bif.type = type;
             bif.id = ++lastID;
             bif.startTime = DateTime.Now.Ticks;
-            bif.finishTime = DateTime.Now.AddSeconds(lastID).Ticks;
+            bif.finishTime = DateTime.Now.AddSeconds(lastID + 5).Ticks;
+            bif.xStartPos = xStartPos;
+            bif.xSpeed = xSpeed;
             bullets.Add(bif.id, bif);
         }
     }
