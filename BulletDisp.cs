eb790f857bf344cae444463995408b32318c4cef
Pisces000221 <1786762946@qq.com>
2015-12-04 23:31:30 +0800

    Display comments that are hand-drawn pictures

--- BulletDisp.cs
+++ BulletDisp.cs
@@ -9,21 +9,39 @@ namespace TwoDoubleThree {
         public CustomLabel() {
             OutlineForeColor = Color.Green;
             OutlineWidth = 2;
+            IsDrawingMode = false;
         }
         public Color OutlineForeColor { get; set; }
         public float OutlineWidth { get; set; }
+        public bool IsDrawingMode { get; set; }
+        public int DrawWidth, DrawHeight;
+        public byte[, ] Pixels;
+        private Bitmap drawnImage;
+        public void InitializeImage() {
+            if (this.IsDrawingMode) {
+                drawnImage = new Bitmap(DrawWidth, DrawHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                for (int i = 0; i < DrawWidth; ++i)
+                    for (int j = 0; j < DrawHeight; ++j)
+                        if (Pixels[i, j] == 1) drawnImage.SetPixel(i, j, ForeColor);
+                        else drawnImage.SetPixel(i, j, BackColor);
+            }
+        }
         protected override void OnPaint(PaintEventArgs e) {
-            e.Graphics.FillRectangle(new SolidBrush(BackColor), ClientRectangle);
-            GraphicsPath gp = new GraphicsPath();
-            Pen outline = new Pen(OutlineForeColor, OutlineWidth) { LineJoin = LineJoin.Round };
-            StringFormat sf = new StringFormat();
-            Brush foreBrush = new SolidBrush(ForeColor);
-            Rectangle r = this.ClientRectangle;
-            gp.AddString(this.Text, Font.FontFamily, (int)Font.Style, Font.Size, r, sf);
-            e.Graphics.ScaleTransform(1.3f, 1.35f);
-            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
-            e.Graphics.DrawPath(outline, gp);
-            e.Graphics.FillPath(foreBrush, gp);
+            if (this.IsDrawingMode) {
+                e.Graphics.DrawImage(drawnImage, new PointF(0, 0));
+            } else {
+                e.Graphics.FillRectangle(new SolidBrush(BackColor), ClientRectangle);
+                GraphicsPath gp = new GraphicsPath();
+                Pen outline = new Pen(OutlineForeColor, OutlineWidth) { LineJoin = LineJoin.Round };
+                StringFormat sf = new StringFormat();
+                Brush foreBrush = new SolidBrush(ForeColor);
+                Rectangle r = this.ClientRectangle;
+                gp.AddString(this.Text, Font.FontFamily, (int)Font.Style, Font.Size, r, sf);
+                e.Graphics.ScaleTransform(1.3f, 1.35f);
+                e.Graphics.SmoothingMode = SmoothingMode.Default;
+                e.Graphics.DrawPath(outline, gp);
+                e.Graphics.FillPath(foreBrush, gp);
+            }
         }
     }
 
@@ -62,14 +80,50 @@ namespace TwoDoubleThree {
                     value = BackgroundReplacement;
                 }
                 label.ForeColor = value;
+                label.InitializeImage();
             }
         }
 
         public new String Text {
             get { return label.Text; }
             set {
-                label.Text = value.Replace(' ', '　');
-                this.Size = label.Size;
+                // #WIDTH HEIGHT WHITES BLACKS WHITES BLACKS...
+                // WHITE -> Transparent, BLACK -> Solid
+                if (value.StartsWith("#")) {
+                    if (value.StartsWith("##")) {
+                        label.IsDrawingMode = false;
+                        label.Text = value.Substring(1).Replace(' ', '　');
+                        this.Size = label.Size;
+                    } else {
+                        string[] s = value.Substring(1).Split(' ');
+                        if (s.Length < 2) {
+                            Console.WriteLine("ERROR: Width and height must be provided to a bullet.");
+                            return;
+                        }
+                        int[] n = new int[s.Length];
+                        for (int i = 0; i < s.Length; ++i) n[i] = int.Parse(s[i]);
+                        if (n[0] <= 0 || n[1] <= 0) {
+                            Console.WriteLine("ERROR: Invalid width/height.");
+                            return;
+                        }
+                        label.IsDrawingMode = true;
+                        label.DrawWidth = n[0];
+                        label.DrawHeight = n[1];
+                        label.Pixels = new byte[n[0], n[1]];
+                        int x = 0, y = 0;
+                        for (int i = 2; i < n.Length; ++i) for (int j = 0; j < n[i]; ++j) {
+                            label.Pixels[y, x] = (byte)(i % 2);
+                            if (++y == n[0]) { y = 0; ++x; }
+                        }
+                        label.InitializeImage();
+                        label.Text = value; // ... Or the optimization applied will disable OnPaint()
+                        this.Size = new Size(n[0], n[1]);
+                    }
+                } else {
+                    label.IsDrawingMode = false;
+                    label.Text = value.Replace(' ', '　');
+                    this.Size = label.Size;
+                }
             }
         }
 
