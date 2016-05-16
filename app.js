239bf64aeb8456d1a47385744c097eae36973731
Pisces000221 <1786762946@qq.com>
2016-05-16 20:03:09 +0800

    Add necessary packages

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const redis = require('redis'),
+    client = redis.createClient();
+
+const IO = require('koa-socket');
+const io = new IO();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/fuwa');
+
+client.set('string__key', 'string value');
+client.get('string__key', (err, reply) => { console.log(reply); });
+client.hset('hash__key', 'field one', 'value one');
+client.hset('hash__key', 'field two', 'value two');
+client.hget('hash__key', 'field two', (err, reply) => { console.log(reply); });
+
+io.attach(app);
+io.on('connection', (ctx, data) => {
+    console.log('Someone connected! Welcome -u-', data);
+});
+io.on('poke', (ctx, data) => {
+    console.log('Giggle.', data);
+});
+
 app.use(async (ctx, next) => {
     const start = Date.now();
     await next();
@@ -10,7 +33,7 @@ app.use(async (ctx, next) => {
 });
 
 app.use(ctx => {
-    ctx.body = 'Hello world!';
+    ctx.body = '<html><body><script src="https://cdn.socket.io/socket.io-1.4.5.js"></script></body></html>';
 });
 
 app.listen(2233);
