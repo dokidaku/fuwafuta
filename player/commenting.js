964644a3513c77c365adf53cbad0cf395e4838d5
Pisces000221 <1786762946@qq.com>
2016-11-27 18:11:31 +0800

    Add buttons and fix commenting toolbar

--- player/commenting.js
+++ player/commenting.js
@@ -1,12 +1,27 @@
 function commentingPlugin (options) {
-  this.on('play', function (e) {
+  var Button = videojs.getComponent('Button');
+
+  this.on('loadeddata', function (e) {
     console.log('Playback started!');
     var fscrCtrl = document.getElementsByClassName('vjs-fullscreen-control')[0];
+
     var textArea = document.createElement('input');
-    textArea.className += 'vjs-cmtinput';
+    textArea.className += ' vjs-cmtinput';
     textArea.setAttribute('placeholder', 'Start commenting here = =');
     textArea.setAttribute('maxlength', '140');
     this.controlBar.addChild(textArea);
     player.controlBar.el().insertBefore(textArea, fscrCtrl);
+
+    var sendBtn = new Button();
+    sendBtn.el().className += ' vjs-icon-circle-inner-circle';
+    sendBtn.el().setAttribute('title', 'Send');
+    sendBtn.on('click', function () { alert('x'); });
+    player.controlBar.el().insertBefore(sendBtn.el(), fscrCtrl);
+
+    var cmtToggleBtn = new Button();
+    cmtToggleBtn.el().className += ' vjs-icon-subtitles';
+    cmtToggleBtn.el().setAttribute('title', 'Comments Display');
+    cmtToggleBtn.on('click', function () { alert('x'); });
+    player.controlBar.el().insertBefore(cmtToggleBtn.el(), fscrCtrl);
   });
 };
