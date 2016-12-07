c9241c45f7c4adbfd63e8eb3cfc6f08854675e6e
Pisces000221 <1786762946@qq.com>
2016-12-07 18:34:08 +0800

    Use --require babel-register flags for Mocha

--- cmtsvr/test/test.js
+++ cmtsvr/test/test.js
@@ -1,3 +1,16 @@
-require('babel-core/register');
+const foo = async (x) => x + 1
+const assert = require('assert')
 
-const test = require('./testcases');
+describe('Synchronous tests', () => {
+  it('should work', () => {
+    const x = 3 + 1
+    assert.equal(x, 4)
+  })
+})
+
+describe('Asynchronous tests', () => {
+  it('should work', async () => {
+    const x = await foo(3)
+    assert.equal(x, 4)
+  })
+})
